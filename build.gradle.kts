e1a9702d579ea30f404b0aca8881879b5bd3d8b0
ooexiaoo <varunpaherwar@proton.me>
2026-09-21 21:39:06 +0530

    Fix KSP version: 2.1.21-2.0.2 exists; 1.0.29 does not

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,5 +3,5 @@ plugins {
     id("org.jetbrains.kotlin.android") version "2.1.21" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.1.21" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "2.1.21" apply false
-    id("com.google.devtools.ksp") version "2.1.21-1.0.29" apply false
+    id("com.google.devtools.ksp") version "2.1.21-2.0.2" apply false
 }
